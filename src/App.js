499f5d36f30ac7bbd1db5cf87d24cec8adb3717b
RafDes21 <israeltineo84@gmail.com>
2022-04-17 23:13:05 -0300

    agregue nav y style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+
+
 
 function App() {
   return (
     <div className="App">
+       <NavBar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -17,7 +21,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+        </header>
+   
     </div>
   );
 }
